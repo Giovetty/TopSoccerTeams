88bc7e86a3d49642583366d17e6ae319b40f93e4
Giovetty <kelsia20giovetty@gmail.com>
2026-04-10 13:36:34 +0100

    added function to array values

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.topsoccerteams
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,11 +10,21 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.Arrays
 
+fun  LogArrValues(array: Array<String>,limit: Int = 0){
+    if (limit == 0 ) {
+        Log.v( "Array Values", Arrays.toString(array))
+    }else {
+        Log.v ("Array Values", Arrays.toString(
+            array.sliceArray(0..limit -1)))
+    }
+    Log.v("Array values: ", Arrays.toString(array))
+}
 class MainActivity : AppCompatActivity() {
 
    val teams =  arrayOf<String> ("Man Sundowns FC","Orlando Pirates","Sex United FC",
        "Stellenbosh FC","Sivest Wits")
 
+//   val names = arrayOf<String>("name1", "name2", "name3")
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,16 +33,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val teamsTxt = findViewById<TextView>(R.id.TeamsView)
+        var teamsDisplay = ""
+        var Count = 0
 
         teams[0] = " Real Madrid"
-        var teamsDisplay = ""
-         var Count = 0
-        while (Count < teams.count()) {
-            teamsDisplay += "${teams[Count]}\n"
-            Count++
 
+        LogArrValues(teams)
+        LogArrValues(teams,3)
+
+//        LogArrValues(names)
+
+
+
+//        while (Count < teams.count()) {
+//            teamsDisplay += "${teams[Count]}\n"
+//            Count++
+//
+//          }
+        for (team in teams){
+            teamsDisplay += "${teams}\n"
         }
-
 
 
         teamsTxt.text = Arrays.toString(teams)

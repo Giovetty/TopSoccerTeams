2311027c48b0487d8431585d4a66e3cb4fc0b8f2
Giovetty <kelsia20giovetty@gmail.com>
2026-04-15 14:59:36 +0100

    Added function to find the shortest element in array

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -10,46 +10,62 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.Arrays
 //logs the content of an array of strings
-fun  LogArrValues(array: Array<String>,limit: Int = 0){
-    if (limit == 0 ) {
-        Log.v( "Array Values", Arrays.toString(array))
-    }else {
-        Log.v ("Array Values", array.sliceArray
-            (0..limit - 1).contentToString())
+//fun  LogArrValues(array: Array<String>,limit: Int = 0){
+//    if (limit == 0 ) {
+//        Log.v( "Array Values", Arrays.toString(array))
+//    }else {
+//        Log.v ("Array Values", array.sliceArray
+//            (0..limit - 1).contentToString())
+//    }
+//    Log.v("Array values: ", Arrays.toString(array))
+//}
+// Created function to find the shortest element in the array
+
+fun getShortestString(array: Array<String>): String {
+    var shortestLength = 0
+    var shortestElement = ""
+    for (element in array){
+        if (element.count()> shortestLength){
+         shortestLength = element.count()
+            shortestElement = element
+        }
     }
-    Log.v("Array values: ", Arrays.toString(array))
+    return shortestElement
 }
 
-
 class MainActivity : AppCompatActivity() {
-
+// Array storing soccer teams
    val teams =  arrayOf<String> ("Man Sundowns FC","Orlando Pirates","Sex United FC",
        "Cape Town City FC","Bidvest Wits FC")
 
-//   val names = arrayOf<String>("name1", "name2", "name3")
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-// added teams text view
+        // Linking the TextView to Kotlin
         val teamsTxt = findViewById<TextView>(R.id.TeamsView)
+        // Variable to store the list of teams
         var teamsDisplay = ""
+        //Count variable
         var Count = 0
+
+        //Updating the first element of the array
 
         teams[0] = " Real Madrid"
 // call the method to log to array values
-        LogArrValues(teams)
-        LogArrValues(teams,3)
+//        LogArrValues(teams)
+//        LogArrValues(teams,3)
+
 
         //Added loop to all teams to the display string
-
         for (team in teams){
             teamsDisplay += "${teams}\n"
+
         }
 
-
+       //Display the final result in text view
         teamsTxt.text = Arrays.toString(teams)
 
 
